efc06fdd39357fcc900872668a6c36491469ab1c
OlegHarlamov98 <oleg.harlamov@fau.de>
2021-02-18 19:08:02 +0000

    finalize interface + add detail image carousel modal

---
+++ app/resources/js/components/CarouselModal.vue
@@ -0,0 +1,49 @@
+<template>
+    <b-modal id="carouselmodal" title="imgs.ai">
+         <b-carousel
+            id="imgs"
+            v-model="slide"
+            :interval="4000"
+            controls
+            indicators
+            background="#ababab"
+            img-width="1024"
+            img-height="480"
+            style="text-shadow: 1px 1px 2px #333;"
+            @sliding-start="onSlideStart"
+            @sliding-end="onSlideEnd"
+            >
+            <b-carousel-slide
+                v-for="img in imgs"
+                text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+                :img-src="img.url"
+            ></b-carousel-slide>
+        </b-carousel>
+    </b-modal>
+</template>
+
+<script>
+export default {
+    name: 'SearchPanel',
+
+    data : () => ({
+        slide: 0,
+        sliding: null
+    }),
+
+    computed: {
+        imgs() {
+            return this.$parent.imgs
+        }
+    },
+
+    methods: {
+      onSlideStart(slide) {
+        this.sliding = true
+      },
+      onSlideEnd(slide) {
+        this.sliding = false
+      }
+    }
+}
+</script>
