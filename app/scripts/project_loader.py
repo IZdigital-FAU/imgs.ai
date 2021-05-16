aaa9f330e6128485904ef85868a6b09f63a18fac
Oleg Harlamov <61985465+OlegHarlamov98@users.noreply.github.com>
2021-05-16 18:58:08 +0200

    make vector data dir on project create

--- app/scripts/project_loader.py
+++ app/scripts/project_loader.py
@@ -6,6 +6,7 @@ from ..models.imagemetadata import ImageMetadata
 
 from os.path import join
 from os import listdir
+import os
 
 
 def load_project():
@@ -24,8 +25,9 @@ def load_project():
             continue
 
         project.data = [ImageMetadata(**{'name': img}) for img in imgs]
-
         project.save()
+
+        os.makedirs(join(env.VECTORS_DIR, project.name), exist_ok=True)
 
 
 if __name__ == '__main__':
